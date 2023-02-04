aacf9f52629b1781e1fd8018f25724818829efac
saufts <l.szwaczka90@gmail.com>
2023-02-04 20:48:41 +0100

    Finish operate function, taking 3 arguments - an operator and pair of numbers

--- calculator.js
+++ calculator.js
@@ -1,15 +1,39 @@
-const add = function(a,b) {
+const add = function(a, b) {
     return a + b;
 };
 
-const subtract = function(a,b) {
+const subtract = function(a, b) {
     return a - b;
 };
 
-const multiply = function(a,b) {
+const multiply = function(a, b) {
     return a * b;
 };
 
-const divide = function(a,b) {
+const divide = function(a, b) {
     return a / b;
 };
+
+const operate = function(operator, a, b) {
+    switch(operator) {
+        case('+'): {
+            return add(a,b);
+        }    
+        break;
+
+        case('-'): {
+            return subtract(a,b);
+        }
+        break;
+
+        case('*'): {
+            return multiply(a,b);
+        }
+        break;
+
+        case('/'): {
+            return divide(a,b);
+        }
+        break;
+    }
+}
