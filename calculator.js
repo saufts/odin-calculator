a70cb2041e3f89bb9fb29b76931374e4ba22c6a8
saufts <l.szwaczka90@gmail.com>
2023-02-06 23:01:40 +0100

    Add eventListener to 5 buttons + test simple addition with two numbers

--- calculator.js
+++ calculator.js
@@ -1,3 +1,58 @@
+let outputDisplay = document.querySelector('.output-display');
+outputDisplay.textContent = ``;
+
+
+let numTmp = 0;
+numCounter = 0;
+
+let num1 = 0;
+let num2 = 0;
+
+let sign = '';
+
+const dig1 = document.querySelector('#dig1');
+const dig2 = document.querySelector('#dig2');
+
+const equalsSign = document.querySelector('#equals');
+const plusSign = document.querySelector('#plus');
+const clearSign = document.querySelector('#clear');
+
+dig1.addEventListener('click', () => {
+    outputDisplay.textContent += '1';
+    numTmp += '1';
+    //numCounter++;
+    //if(numCounter > 1) numCounter = 0;
+
+});
+
+dig2.addEventListener('click', () => {
+    outputDisplay.textContent += '2';
+    numTmp += '2';
+    //numCounter++;
+    //if(numCounter > 1) numCounter = 0;
+});
+
+plusSign.addEventListener('click', () => {
+    num1 = parseFloat(numTmp);
+    numTmp = 0;
+    outputDisplay.textContent += '+';
+    sign = '+';   
+});
+
+
+equalsSign.addEventListener('click', () => {
+    num2 = parseFloat(numTmp);
+    numTmp = 0;
+    outputDisplay.textContent = operate(sign, num1, num2);
+});
+
+clearSign.addEventListener('click', () => {
+    outputDisplay.textContent = '';
+    sign = '';
+    num1 = 0;
+    num2 = 0;
+});
+
 const add = function(a, b) {
     return a + b;
 };
